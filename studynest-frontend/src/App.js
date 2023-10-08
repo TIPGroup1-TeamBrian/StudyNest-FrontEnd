f1825ded12317e90df1f1c34fa4d5f6ae1a93ad2
Brian Zhang <51120720+zhzizhou@users.noreply.github.com>
2023-10-08 18:21:40 +1100

    Updated Router for student home page

--- studynest-frontend/src/App.js
+++ studynest-frontend/src/App.js
@@ -1,19 +1,18 @@
 import './style/App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
+import RegisterStudent from "./pages/RegisterStudent";
+import Login from "./pages/LoginRouter";
+import LoginComponent from "./components/LoginComponent";
 import StudentHomeComponent from './components/StudentHomeComponent';
-import RegisterStudent from './pages/RegisterStudent';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/StudentHome' element={<StudentHomeComponent/>}/>
         <Route path='/login/RegisterStudent' element={<RegisterStudent/>}/>
-
-        
-        
+        <Route path="/StudentHome" element={<StudentHomeComponent/>}/>
       
       </Routes>
     
