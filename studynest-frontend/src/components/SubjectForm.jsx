e1dff09877df8febaed0f1449f341d2c50629b71
Tharusha Virajith <tvwijesena@gmail.com>
2023-10-12 20:46:49 +1100

    updated subject form page

--- studynest-frontend/src/components/SubjectForm.jsx
+++ studynest-frontend/src/components/SubjectForm.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from 'react';
+
 
 const SubjectForm = () => {
   return (
     <div class="flex justify-end font-Effra font-normal">
            
-            <div class="w-[77%] border-black border-[5px] felx flex-col items-center h-screen bg-[#f7f7f7]">
-                <div class="mt-[6rem] ml-[4rem]">
-                    <h1 class="text-4xl my-3">Subjects</h1>
-                    <form class="bg-white rounded-3xl w-full h-1/2 p-[4rem] ">
-                    <h1>Enrolled Subjects</h1>
-                        <div class="flex flex-row justify-around">
+            <div class="w-[77%] felx flex-col items-center h-screen bg-[#f7f7f7]">
+                <div class="mt-[6rem] mx-[4rem]">
+                    <h1 class="text-4xl my-3 pb-[3rem]">Subjects</h1>
+                    <form class="bg-white rounded-3xl w-full h-1/2 p-[4rem] shadow-lg ">
+                    <h1 class="mb-[2rem] text-xl">Enrolled Subjects</h1>
+                    <div class="flex flex-col">
+                        <div class="flex flex-row justify-around mb-3">
                             <label for="id" hidden="hidden">Computer Science</label>
                             <button type="button" name="cs" id="cs" class="bg-white hover:bg-[#176AE6] hover:text-white text-[#8B8B8B] shadow-lg py-2 px-4 rounded-full ">Computer Science</button>
                             <label for="id" hidden="hidden">Computer Science</label>
@@ -18,22 +20,31 @@ const SubjectForm = () => {
                             <button type="button" name="cs" id="cs" class="bg-white hover:bg-[#176AE6] hover:text-white text-[#8B8B8B] shadow-lg py-2 px-4 rounded-full ">Computer Science</button>
                             <label for="id" hidden="hidden">Computer Science</label>
                             <button type="button" name="cs" id="cs" class="bg-white hover:bg-[#176AE6] hover:text-white text-[#8B8B8B] shadow-lg py-2 px-4 rounded-full ">Computer Science</button>
-                            <label for="id" hidden="hidden">Computer Science</label>
-                            <button type="button" name="cs" id="cs" class="bg-white hover:bg-[#176AE6] hover:text-white text-[#8B8B8B] shadow-lg py-2 px-4 rounded-full ">Computer Science</button>
-                            <label for="id" hidden="hidden">Computer Science</label>
-                            <button type="button" name="cs" id="cs" class="bg-white hover:bg-[#176AE6] hover:text-white text-[#8B8B8B] shadow-lg py-2 px-4 rounded-full ">Computer Science</button>
-                         
+   
                         </div>
+                        {/* <div class="flex flex-row justify-around mb-3">
+                            <label for="id" hidden="hidden">Computer Science</label>
+                            <button type="button" name="cs" id="cs" class="bg-white hover:bg-[#176AE6] hover:text-white text-[#8B8B8B] shadow-lg py-2 px-4 rounded-full ">Computer Science</button>
+                            <label for="id" hidden="hidden">Computer Science</label>
+                            <button type="button" name="cs" id="cs" class="bg-white hover:bg-[#176AE6] hover:text-white text-[#8B8B8B] shadow-lg py-2 px-4 rounded-full ">Computer Science</button>
+                            
+                        </div> */}
+                        <div class="text-center mt-[3rem]">
+                            <a href="" class="inline-block hover:scale-110 transition-transform duration-300 ease-in-out">
+                                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 w-[10rem] h-[3rem] focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-lg px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Unenrol</button>
+                            </a>
+                        </div>
+                    </div>
                        
-                             
-
+                       
+                            
                     </form>
 
                 </div>
 
                 <div>
-                <div class="mt-[2rem] ml-[4rem]">
-                    <form class="bg-white rounded-3xl w-full h-1/2 p-[4rem] ">
+                <div class="mt-[2rem] mx-[4rem] ">
+                    <form class="bg-white rounded-3xl w-full h-1/2 p-[4rem] shadow-xl">
                     <h1>Available Subjects</h1>
                         <div class="flex flex-row justify-around">
                             <label for="id" hidden="hidden">Computer Science</label>
